64713b9c8fc985feb874e7327f0c2114f5b7fa41
Leebumju <dlqkawns@naver.com>
2022-04-13 20:00:30 +0900

    컬렉션뷰 연습

--- MyAlbum/CollectionViewController.swift
+++ MyAlbum/CollectionViewController.swift
@@ -6,18 +6,63 @@
 //
 
 import UIKit
+import Photos
 
-class CollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class CollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate,PHPhotoLibraryChangeObserver {
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    var fetchResult: PHFetchResult<PHAsset>!
+    let imageManager: PHCachingImageManager = PHCachingImageManager()
     var numberOfCell: Int = 10
     let cellIdentifier: String = "photoCell"
     
+    
+    func collectionView(_ collectionView: UICollectionView, canEditItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    
+    
+    
+    
+    
+    
+    func photoLibraryDidChange(_ changeInstance: PHChange) {
+        guard let changes = changeInstance.changeDetails(for: fetchResult)
+            else { return }
+        
+        fetchResult = changes.fetchResultAfterChanges
+        
+        OperationQueue.main.addOperation {
+            self.collectionView.reloadSections(IndexSet(0...0))
+        }
+    }
+    
+    func requesCollection() {
+        let cameraRoll: PHFetchResult<PHAssetCollection> = PHAssetCollection.fetchAssetCollections(with: .smartAlbum, subtype: .smartAlbumUserLibrary, options: nil)
+        
+        guard let cameraRollCollection = cameraRoll.firstObject else {
+            return
+        }
+        
+        let fetchOptions = PHFetchOptions()
+        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+        self.fetchResult = PHAsset.fetchAssets(in: cameraRollCollection, options: fetchOptions)
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.numberOfCell
+        return self.fetchResult?.count ?? 0
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let photoCell: UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: self.cellIdentifier, for: indexPath)
+        let photoCell: FriendCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: self.cellIdentifier, for: indexPath) as! FriendCollectionViewCell
+        
+        let asset: PHAsset = fetchResult.object(at: indexPath.row)
+        
+        imageManager.requestImage(for: asset, targetSize: CGSize(width: 30, height: 30), contentMode: .aspectFill, options: nil, resultHandler: { image, _ in
+            photoCell.photoImageView?.image = image
+        })
+        
         return photoCell
     }
     
@@ -31,6 +76,35 @@ class CollectionViewController: UIViewController, UICollectionViewDataSource, UI
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let photoAuthorizationStatus = PHPhotoLibrary.authorizationStatus()
+        
+        switch photoAuthorizationStatus {
+        case .authorized:
+            print("접근 허가됨")
+            self.requesCollection()
+            self.collectionView.reloadData()
+        case .denied:
+            print("접근 불허")
+        case .notDetermined:
+            print("아직 응답하지 않음")
+            PHPhotoLibrary.requestAuthorization({ (status) in
+                switch status {
+                case .authorized:
+                    print("사용자가 허용함")
+                    self.requesCollection()
+                    self.collectionView.reloadData()
+                case .denied:
+                    print("사용자가 불허함")
+                default: break
+                }
+            })
+        case .restricted:
+            print("접근 제한")
+        case .limited:
+            print("접근 제한")
+        }
+        
+        PHPhotoLibrary.shared().register(self)
         // Do any additional setup after loading the view.
     }
     
---
+++ MyAlbum/FriendCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  FriendCollectionViewCell.swift
+//  MyAlbum
+//
+//  Created by 이범준 on 2022/04/13.
+//
+
+import UIKit
+
+class FriendCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var nameAgeLabel: UILabel!
+    @IBOutlet var addressLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+}
